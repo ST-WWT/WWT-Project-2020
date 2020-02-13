2f02aafec25f66aacd9c5b7f3394c585e93e2f2d
kryptonymous <kryptonymouspowerplayer@gmail.com>
2020-02-13 08:35:12 -0600

    Fixed images

--- React/graphite/src/components/Header.js
+++ React/graphite/src/components/Header.js
@@ -1,20 +1,17 @@
 import React from "react";
 
-const LOGO = "../../public/img/graphite_logo_new.png";
-const PROFILE_ICON = "../../public/img/profile.png";
-
 const Header = (props) => {
   return (
     <nav className="navbar stick-top" colorOnScroll="100">
     <div className="profile">
-      <img src={PROFILE_ICON} alt="Profile icon" />
+      <img src={'./img/profile.png'} alt="Profile icon" />
       <div className="notifications">
         <span className="notification n-red">
           4
         </span>
       </div>
     </div>
-    <img src={LOGO} alt="Graphite logo" className="noselect logo" draggable="false" />
+    <img src={'./img/graphite_logo_new.png'} alt="Graphite logo" className="noselect logo" draggable="false" />
   </nav>
   );
 };
